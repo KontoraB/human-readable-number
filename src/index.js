6345e96f6b5683139c12cbbaf32ad2df3d78047a
Oleksandr Boitsov <kontorab@gmail.com>
2020-10-22 15:45:44 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 module.exports = function toReadable (number) {
-    let arrOne = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-  "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let arrayOne = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+  "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+        arrayTwo = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
   if (number < 20) {
-      return arrOne[number];
+      return arrayOne[number];
   }
 
-  let arrTwo = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-
   if (number < 100) {
-      return arrTwo[Math.floor(number / 10) - 2] + (number % 10 !== 0 ? " " + toReadable(number % 10) : "");
+      return arrayTwo[Math.floor(number / 10) - 2] + (number % 10 !== 0 ? " " + toReadable(number % 10) : "");
   }
 
   if (number < 1000) {
-      return arrOne[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+      return arrayOne[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
   
 }
